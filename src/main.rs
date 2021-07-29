6c90f619fe71394d1af25bdc47d0f737a617aad6
Scott Mabin <scott@mabez.dev>
2021-07-29 15:38:47 +0100

    Add support for changing the kvm switch back input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use std::time::Duration;
+//! M27 KVM control
+//! 
+//! A simple tool to manage the GIGABYTE M27Q KVM interface
+//! Switching to USB-C input is not supported via DDC, but it is exposed via the monitors usb billboard device.
+//! 
+//! All USB writes have been reverse engineered from the OSD side kick tool by analyzing the USB packets over wireshark. 
+
+
+
+use std::{fmt::Display, str::FromStr, thread::sleep, time::Duration};
 
 use rusb::{
     Context, Device, DeviceDescriptor, DeviceHandle, UsbContext,
@@ -7,13 +16,61 @@ use rusb::{
 pub const M27Q_VID: u16 = 0x2109;
 pub const M27Q_PID: u16 = 0x8883;
 
+pub enum KvmInput {
+    Hdmi1 = 0x00,
+    Hdmi2 = 0x01,
+    DisplayPort = 0x02,
+}
+
+impl Display for KvmInput {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            KvmInput::Hdmi1 => write!(f, "HDMI1"),
+            KvmInput::Hdmi2 => write!(f, "HDMI2"),
+            KvmInput::DisplayPort => write!(f, "DP"),
+        }
+    }
+}
+
+impl FromStr for KvmInput {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "HDMI1" => KvmInput::Hdmi1,
+            "HDMI2" => KvmInput::Hdmi2,
+            "DP" => KvmInput::DisplayPort,
+            _ => return Err("Invalid KVM input - valid inputs are HDMI1,HDMI2,DP")
+        })
+    }
+}
+
+struct Args {
+    switch_back_input: Option<KvmInput>,
+    run_trigger: bool
+}
+
 fn main() {
+    let mut pargs = pico_args::Arguments::from_env();
+
+    let args = Args {
+        switch_back_input: pargs.opt_value_from_str("--switch-back-input").unwrap(),
+        run_trigger: pargs.value_from_str("--kvm-trigger").unwrap_or(true),
+    };
+
     match Context::new() {
         Ok(mut context) => match open_device(&mut context, M27Q_VID, M27Q_PID) {
             Some((_device, _device_desc, handle)) => {
                 println!("Succesfully opened m27q connection!");
-                println!("Triggering KVM switch...");
-                handle.write_control(0x40, 178, 0, 0, &[0x6e, 0x51, 0x84, 0x03, 0xe0, 0x69, 0x01], Duration::from_secs(3)).unwrap();
+                if let Some(input) = args.switch_back_input {
+                    println!("Input switch supplied, writing {} to to kvm switch back", input);
+                    handle.write_control(0x40, 178, 0, 0, &[0x6e, 0x51, 0x84, 0x03, 0xe0, 0x6b, input as u8], Duration::from_secs(3)).unwrap();
+                    sleep(Duration::from_millis(50))
+                }
+                if args.run_trigger {
+                    println!("Triggering KVM switch...");
+                    handle.write_control(0x40, 178, 0, 0, &[0x6e, 0x51, 0x84, 0x03, 0xe0, 0x69, 0x01], Duration::from_secs(3)).unwrap();
+                }
                 println!("Success!");
             }
             None => println!("could not find m27q"),
